02eb18d997afebaed1f587d56325cfe029057ac3
Papon S. <>
2025-04-20 16:06:23 +0700

    [Feat] initial middleware

--- server/server.go
+++ server/server.go
@@ -8,9 +8,12 @@ import (
 	"os/signal"
 	"sync"
 	"syscall"
+	"time"
 
 	"github.com/JARNBOY/jb-isekai-shop-tutorial/config"
+	"github.com/labstack/echo/middleware"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 	"gorm.io/gorm"
 )
@@ -42,10 +45,19 @@ func NewEchoServer(conf *config.Config, db *gorm.DB) *echoServer {
 }
 
 func (s *echoServer) Start() {
+	corsMiddleware := getCORSMiddleware(s.conf.Server.AllowOrigins)
+	bodyLimitMiddleware := getBodyLimitMiddleware(s.conf.Server.BodyLimit)
+	timeOutMiddleware := getTimtimeOutMiddleware(s.conf.Server.TimeOut)
+
+	s.app.Use(middleware.Recover())
+	s.app.Use(middleware.Logger())
+	s.app.Use(corsMiddleware)
+	s.app.Use(bodyLimitMiddleware)
+	s.app.Use(timeOutMiddleware)
 
 	s.app.GET("/v1/health", s.healthCheck)
 
-	quitCh := make(chan os.Signal, 1)\
+	quitCh := make(chan os.Signal, 1)
 	signal.Notify(quitCh, syscall.SIGINT, syscall.SIGTERM)
 	go s.gracefullyShutdown(quitCh)
 	s.httpListening()
@@ -72,4 +84,26 @@ func (s *echoServer) gracefullyShutdown(quitCh chan os.Signal) {
 
 func (s *echoServer) healthCheck(c echo.Context) error {
 	return c.String(http.StatusOK, "OK")
+}
+
+func getTimeOutMiddlewre(timeout: time.Duration) echo.MiddlewareFunc {
+	return middleware.TimeoutWithConfig(middleware.TimeoutConfig{
+		Skipper: 		middleware.DefaultSkipper,
+		ErrorMessage:	"Request Timeout",
+		Timeout: 		timeout * time.Second
+	})
+}
+
+
+func getCORSMiddleware(allowOrigin []string) echo.MiddlewareFunc {
+	return middleware.CORSWithConfig(middleware.CORSConfig{
+		Skipper: 		middleware.DefaultSkipper,
+		AllowOrigin: 	allowOrigin,
+		AllowMethods: 	[]string{echo.GET, echo.POST, echo.PUT, echo.PATCH, echo.DELETE},
+		AllowHeaders:	[]string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	})
+}
+
+func getBodyLimitMiddleware(bodyLimit string) echo.MiddlewareFunc {
+	return middleware.BodyLimit(bodyLimit)
 }
